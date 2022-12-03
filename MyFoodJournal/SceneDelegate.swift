270c0c0d6babd7b8f272e62938f2e7bd69dee420
eliascreates <thapeloelias.kekana@gmail.com>
2022-12-03 11:55:18 +0200

    Add Core Data into the App

--- MyFoodJournal/SceneDelegate.swift
+++ MyFoodJournal/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  MyFoodJournal
 //
-//  Created by IACD 013 on 2022/11/30.
+//  Created by IACD 013 on 2022/12/03.
 //
 
 import UIKit
@@ -45,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
---
+++ MyFoodJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFoodJournal
+//
+//  Created by IACD 013 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
